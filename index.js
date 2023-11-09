5cae2ba3914f023f8f1f0097f5cc0dcfe2626060
Madhu-R-cloud <madhusudanr00@gmail.com>
2023-11-09 15:55:05 +0530

    Merge remote changes into Dev branch

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT ||  4000;
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/bookstore', {
+   
+})
+.then(()=>console.log("Connection Successfull..."))
+.catch((err)=>console.log(err));
+
+const db = mongoose.connection;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const bookRoutes = require('./routes/BookRoutes'); 
+
+app.use('/books', bookRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
